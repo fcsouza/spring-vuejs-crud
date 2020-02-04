b1bfe92517f3d51dd2e4fc8b857a48ad8e5ab247
fcsouza <fcsouza18@hotmail.com>
2020-02-04 16:41:16 -0300

    adicionando pages e controllers

---
+++ src/main/java/pages/RoleController.java
@@ -0,0 +1,21 @@
+package pages;
+
+
+import infrastructure.RoleRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RoleController {
+
+    private final RoleRepository roleRepository;
+
+    public RoleController(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @GetMapping("/roles")
+    public String rolesPage() {
+        return "roles";
+    }
+}
---
+++ src/main/java/pages/UserController.java
@@ -0,0 +1,22 @@
+package pages;
+
+
+import infrastructure.UserRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    private final UserRepository personRepository;
+
+    public UserController(UserRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @GetMapping("/users")
+    public String users() {
+        return "users";
+    }
+}
